fc1f3c34392abf4861e277fd10969fc52fd695c0
Oliver Markowski <oliver@fullblownimages.com>
2018-03-06 06:53:33 +0100

    talking to pmd seems working...off for field testing now!

--- Application/src/main/java/com/example/android/camera2basic/Camera2BasicFragment.java
+++ Application/src/main/java/com/example/android/camera2basic/Camera2BasicFragment.java
@@ -998,14 +998,13 @@ public class Camera2BasicFragment extends Fragment
         protected String doInBackground(String... params) {
             //String hostname = "192.168.9.126";
             //String hostname = "192.168.43.123";
-            String hostname = "lightstage";
+            String hostname = "lightstage"; // TODO: put hostname into UI - and remember the last one used
             Socket lightstageClientSocket = null;
             int lightStagePort=50007;
             Socket pmdClientSocket = null;
             int pmdPort=50008;
 
-            pictureSession = getCurrentTimeStamp();
-            pictureCounter=0;
+
 
             try {
                 try {
@@ -1028,7 +1027,7 @@ public class Camera2BasicFragment extends Fragment
                 lightstageOutStream = new DataOutputStream(lightstageClientSocket.getOutputStream());
                 pmdOutStream = new DataOutputStream(pmdClientSocket.getOutputStream());
                 lightstageInputStream = new DataInputStream(lightstageClientSocket.getInputStream());
-//                pmdInputStream = new DataInputStream(pmdClientSocket.getInputStream());
+                pmdInputStream = new DataInputStream(pmdClientSocket.getInputStream());
 
                 // Send first message
                 lightstageOutStream.writeByte(1); // INIT LIGHTSTAGE
@@ -1036,8 +1035,16 @@ public class Camera2BasicFragment extends Fragment
                 lightstageOutStream.flush(); // Send off the data
                 pmdOutStream.flush(); // Send off the data
 
-
                 Log.d(TAG, "waiting for lightstage & pmd");
+
+                // wait for pmd to answer
+                if (pmdInputStream.readByte()==1) { // INIT PMD
+                    Log.d(TAG, "pico ready!");
+                }
+                pictureSession = getCurrentTimeStamp();
+                Log.d(TAG, "timestamp: " + pictureSession );
+                pictureCounter=0;
+
                 while (true) {
                     int command = lightstageInputStream.readByte();
                     if (command == 2) {
